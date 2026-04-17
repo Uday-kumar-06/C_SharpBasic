6430505a70da00bdf729b199acaab649d8639e93
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-04-17 13:57:45 +0530

    some code is added

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -2,5 +2,12 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Boxing and Unboxing demo in C#");
+        //value type variables 
+        int a = 10;
+        //Boxing : Converting value type to reference type
+        object obj = a; // Boxing the integer value into an object
+                        //Object is the gloabal base class in C# and all the types in C# are derived from object class
+        Console.WriteLine(obj);
     }
 }
